48a39372a750a4a9d6a34b5b30858b545bcf107d
Harold Angenent <harold@trendwerk.nl>
2016-08-02 14:21:52 +0200

    Also check for classes

--- Submenu.php
+++ Submenu.php
@@ -28,13 +28,22 @@ final class Submenu
         if (! $this->active) {
             $menu = new Menu($this->location);
 
-            $activeItems = array_filter($menu->items, function ($item) {
-                return ($item->current || $item->current_item_parent || $item->current_item_ancestor);
-            });
-
+            $activeItems = array_filter($menu->items, [$this, 'isCurrent']);
             $this->active = array_shift($activeItems);
         }
 
         return $this->active;
+    }
+
+    private function isCurrent($item)
+    {
+        return ($item->current || $item->current_item_parent || $item->current_item_ancestor || $this->hasCurrentClasses($item->classes));
+    }
+
+    private function hasCurrentClasses($classes)
+    {
+        $currentClasses = ['current-menu-item', 'current-menu-parent', 'current-menu-ancestor'];
+
+        return !! count(array_intersect($currentClasses, $classes));
     }
 }
